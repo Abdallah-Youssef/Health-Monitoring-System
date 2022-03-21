ccaf874e5374042685d992f454d7762d23e5d4c2
Abdallah Youssef <abdallah.yossf@gmail.com>
2022-03-21 13:54:16 -0700

    Added cpu usage to the health message, command line arguments to choose frequency and service name

--- mockservice/go.mod
+++ mockservice/go.mod
@@ -2,6 +2,8 @@ module example/web-service-gin
 
 go 1.17
 
-require github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58
-
-require github.com/ricochet2200/go-disk-usage/du v0.0.0-20210707232629-ac9918953285 // indirect
+require (
+	github.com/mackerelio/go-osstat v0.2.1
+	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58
+	github.com/ricochet2200/go-disk-usage/du v0.0.0-20210707232629-ac9918953285
+)
--- mockservice/main.go
+++ mockservice/main.go
@@ -1,71 +1,111 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
-	"github.com/pbnjay/memory"  // for ram usage
+	"os"
+	"strconv"
 	"time"
-	"encoding/json"
+
+	"github.com/mackerelio/go-osstat/cpu" // for cpu usage
+	"github.com/pbnjay/memory"            // for ram usage
 	"github.com/ricochet2200/go-disk-usage/du"
 )
 
+var serviceName string = "Default Service Name"
+var freq int // milliseconds
+
 // This struct can be used for both the "RAM" and "DISK" fields
 type Mem struct {
-	Total 	float32
-	Free  	float32
+	Total float32
+	Free  float32
 }
 
 type HealthMessage struct {
-	ServiceName string `json:serviceName`
-	TimeStamp   int64 
-	CPU			float32 
-	RAM			Mem
-	Disk		Mem
+	serviceName string
+	TimeStamp   int64
+	CPU         float32
+	RAM         Mem
+	Disk        Mem
 }
 
 func getRamUsage() Mem {
 	var total float32 = float32(memory.TotalMemory()) / 1024 / 1024 / 1024
 	var free float32 = float32(memory.FreeMemory()) / 1024 / 1024 / 1024
-	return Mem {
+	return Mem{
 		Total: total,
-		Free: free,
+		Free:  free,
 	}
+}
+
+func getCPUUsage() float32 {
+	before, err := cpu.Get()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		return -1
+	}
+	time.Sleep(time.Duration(freq) * time.Millisecond)
+	after, err := cpu.Get()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		return -1
+	}
+
+	total := float32(after.Total - before.Total)
+	return float32(after.System-before.System) / total
 }
 
 func getDiskUsage() Mem {
-	diskUsage := du.NewDiskUsage("D:")
+	diskUsage := du.NewDiskUsage("/")
 
 	total := float32(diskUsage.Size()) / 1024 / 1024 / 1024
 	free := float32(diskUsage.Available()) / 1024 / 1024 / 1024
-	return Mem {
+	return Mem{
 		Total: total,
-		Free: free,
+		Free:  free,
 	}
 }
 
-
-
-func getHealthJSON() HealthMessage{
+func getHealthJSON() HealthMessage {
 	return HealthMessage{
-		ServiceName: "My service",
-		TimeStamp: time.Now().UnixNano(),
-		RAM: getRamUsage(),
-		Disk: getDiskUsage(),
+		serviceName: serviceName,
+		TimeStamp:   time.Now().UnixNano(),
+		CPU:         getCPUUsage(),
+		RAM:         getRamUsage(),
+		Disk:        getDiskUsage(),
 	}
 }
 
+func main() {
+	// Parse frequency
+	if len(os.Args) >= 2 {
+		x, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Printf("Cannot convert \"%s\" to milliseconds\n", os.Args[1])
+			os.Exit(1)
+		}
 
-func main(){
-    conn, err := net.Dial("udp", "127.0.0.1:3500")
-    if err != nil {
-        fmt.Printf("Some error %v", err)
-        return
+		freq = x
+	}
+
+	if len(os.Args) >= 3 {
+		serviceName = os.Args[2]
+	}
+
+	conn, err := net.Dial("udp", "127.0.0.1:3500")
+	if err != nil {
+		fmt.Printf("Some error %v", err)
+		return
 	}
 	defer conn.Close()
 
-	healthMessage := getHealthJSON()
-	str, _ := json.Marshal(healthMessage)
-	
-	fmt.Println(string(str))
-	fmt.Fprintf(conn, string(str))
+	for {
+		healthMessage := getHealthJSON()
+		str, _ := json.Marshal(healthMessage)
+
+		fmt.Println(string(str))
+		fmt.Fprintf(conn, string(str))
+	}
+
 }
