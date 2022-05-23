4667731e50d910ce328e6702e73bda0fe124b5f3
Abdallah <abdallah.yossf@gmail.com>
2022-05-23 22:55:09 +0200

    Replace Avro with Thrift

--- docker/webscheduler/mapreduce/MapReduceJob.java
+++ docker/webscheduler/mapreduce/MapReduceJob.java
@@ -11,30 +11,21 @@ import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.conf.Configuration;
 
-import org.apache.avro.Schema;
-import org.apache.avro.SchemaBuilder;
-import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericRecord;
-
-import org.apache.parquet.avro.AvroParquetOutputFormat;
+import org.apache.parquet.example.data.simple.SimpleGroupFactory;
+import org.apache.parquet.example.data.GroupFactory;
 import org.apache.parquet.example.data.Group;
-
+import org.apache.parquet.hadoop.example.ExampleOutputFormat;
+import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.MessageTypeParser;
 
 public class MapReduceJob {
-    // static final Schema schema = SchemaBuilder.record("HealthMessagesSummary")
-    //                                   .fields()
-    //                                   .name("cpu").type().floatType().noDefault()
-    //                                   .endRecord();
-    static final Schema SCHEMA = new Schema.Parser().parse(
-      "{\n"+
-      "   \"type\": \"record\",\n"+
-      "   \"name\": \"HealthMessagesSummary\",\n"+
-      "   \"fields\": [\n"+
-      "     {\"name\": \"cpu\", \"type\": \"double\"}\n"+
-      "   ]\n" + 
-      " }"
-    );
+    static final MessageType SCHEMA = MessageTypeParser.parseMessageType(
+        "message example {\n" +
+          "required double cpu;\n" +
+        "}");
 
+
+    static final GroupFactory groupFactory = new SimpleGroupFactory(SCHEMA);
 
     public static class Map extends Mapper<LongWritable, Text, Text, HealthMessage> {
         private Text serviceNameKey = new Text();
@@ -54,19 +45,12 @@ public class MapReduceJob {
         }
     }
 
-    public static class TextToAvroParquetMapper extends Mapper<Text, HealthMessage, Void, GenericRecord> {
-      
-      
-      GenericRecord genericRecord = new GenericData.Record(SCHEMA);
-
-        public void map(Text serviceKey, HealthMessage healthMessage, Context context) throws IOException, InterruptedException {
-              // Parse the value yourself here,
-              // calling "put" on the Avro GenericRecord,
-              // once for each field.  The GenericRecord
-              // object is reused for every map call.
-              genericRecord.put("cpu", healthMessage.cpu.get());
-              context.write(null, genericRecord);
-        }
+    public static class ThriftParquetMap extends Mapper {
+      public void map(Text serviceKey, HealthMessage healthMessage, Context context) throws IOException, InterruptedException {
+          Group group = groupFactory.newGroup()
+                        .append("cpu", healthMessage.cpu.get());
+          context.write(serviceKey, group);
+      }
     }
 
 
@@ -101,15 +85,15 @@ public class MapReduceJob {
 
         Job job2 = Job.getInstance(conf, "ParquetFormattingJob");
         job2.setJarByClass(MapReduceJob.class);
-        job2.setMapperClass(TextToAvroParquetMapper.class);
+        job2.setMapperClass(ThriftParquetMap.class);
         job2.setNumReduceTasks(0);
         job2.setOutputKeyClass(Void.class);
         job2.setOutputValueClass(Group.class);
         job2.setInputFormatClass(TextInputFormat.class);
-        job2.setOutputFormatClass(AvroParquetOutputFormat.class);
-        AvroParquetOutputFormat.setSchema(job2, SCHEMA);
+ 
         FileInputFormat.addInputPath(job2, new Path(args[2]));
         FileOutputFormat.setOutputPath(job2, new Path(args[3]));
+        ExampleOutputFormat.setSchema(job2, SCHEMA);
 
         if (!job2.waitForCompletion(true)) {
             System.exit(1);
